758c2421fae749135b428ed22094a26f7c4153fa
aryaom74-max <aryaom74@gmail.com>
2026-04-07 16:24:12 +0700

    Update ArrayStruct.cpp

--- ArrayStruct.cpp
+++ ArrayStruct.cpp
@@ -38,7 +38,7 @@ int main() {
     cout << "Input Kota: ";
     getline(cin, mhsTunggal.alamat.kota);
     
-    cout << "Input Um: ";
+    cout << "Input Umur: ";
     cin >> mhsTunggal.umur;
     cin.ignore(); // Membersihkan sisa 'Enter' agar tidak mengganggu input berikutnya
 
